1cde8d74261f4768c6ff3970343e93cee7bac44e
Komplementariteten <47053492+Komplementariteten@users.norepley.github.com>
2023-12-25 22:59:30 +0100

    Auto commit @25-12-2023 22:59

--- adc_and_log/adc_and_log.cpp
+++ adc_and_log/adc_and_log.cpp
@@ -2,9 +2,10 @@
 
 #include "daisy_seed.h"
 #include "daisysp.h"
+#include <string>
 
-#define NUMBER_OF_FLOATS 1024
-#define TEST_MEMORY_SIZE (NUMBER_OF_FLOATS * 32)
+#define NUMBER_OF_VALUES 1024
+#define TEST_MEMORY_SIZE (NUMBER_OF_VALUES * 16)
 
 using namespace daisy;
 using namespace daisysp;
@@ -13,66 +14,36 @@ static DaisySeed hw;
 static int value_count = 0;
 
 
-void store_value(float value){
+void store_value(uint16_t value){
 	int32_t *ram = (int32_t *)0xC0000000;
 	unsigned char const *p = reinterpret_cast<unsigned char const *>(&value);
-	memcpy((sizeof(float) * value_count) + ram, p, sizeof(float));
+	memcpy((sizeof(uint16_t) * value_count) + ram, p, sizeof(uint16_t));
 	value_count++;
 }
 
-float get_avg_from_ram(){
+void get_avg_from_ram(){
 	int32_t *ram = (int32_t *)0xC0000000;
-	float sum = 0.0;
-	for (size_t i = 0; i < NUMBER_OF_FLOATS; i++)
+	uint64_t sum = 0.0;
+	for (size_t i = 0; i < NUMBER_OF_VALUES; i++)
 	{
-		unsigned char const *p = (unsigned char const *)((sizeof(float) * i) + ram);
-		const float* f = reinterpret_cast<const float*>(p);
-		sum += *f;
-	}
-
-	hw.PrintLine("%d", sum);
-	return (sum / NUMBER_OF_FLOATS);
-}
-
-/** Verify FLT_FMT/FLT_VAR accuracy
- */
-void VerifyFloats()
-{
-    char ref[32];
-    char tst[32];
-    bool result = true;
-    for(float f = -100.0; f < 100.0; f += 0.001)
-    {
-        sprintf(tst, FLT_FMT(3), FLT_VAR(3, f));
-
-        const double f_tst = strtod(tst, nullptr);
-
-        /* verify down to the least significant digit, which is off because of truncation */
-        if(abs((double)f - f_tst) > 1.0e-3)
-        {
-            sprintf(ref, "%.3f", f);
-            hw.PrintLine(
-                "mismatch: ref = %s, dsy = %s, float = %.6f", ref, tst, f);
-            result = false;
-        }
-    }
-    hw.PrintLine("FLT_FMT(3)/FLT_VAR(3) verification: %s",
-                 result ? "PASS" : "FAIL");
-}
-
-
-
-/* float ReadAndLog(DaisySeed& hw, int number_of_values) {
-	// std::vector<float> values;
-	for (auto i = 0; i < number_of_values; i++) {
-		avg += value;
-		// values.push_back(value);
+		uint16_t result;
+		unsigned char const *p = (unsigned char const *)((sizeof(uint16_t) * i) + ram);
+		memcpy(&result, p, sizeof(uint16_t));
+		sum += result;
 		System::Delay(1);
 	}
-	hw.adc.Stop();
-	// System::Delay(1);
-	return avg / number_of_values;
-} */
+	uint16_t avg = sum / NUMBER_OF_VALUES;
+	System::Delay(1);
+}
+
+
+void collect_temperature() {
+		for (size_t i = 0; i < NUMBER_OF_VALUES; i++)
+		{
+			store_value(hw.adc.Get(0));
+			System::Delay(1);
+		}
+}
 
 
 int main(void)
@@ -80,30 +51,21 @@ int main(void)
 	hw.PrintLine("This may be used anywhere too");
 	hw.Configure();
 	hw.Init();
-	hw.StartLog(true);
-	VerifyFloats();
     // hw.PrintLine("Verify CRT floating point format: %.3f.", 123.0f);
-
+	hw.StartLog();
+	hw.PrintLine("Welcom, lets start");
 	Led led1;
 	led1.Init(hw.GetPin(28), false);
-	float avg = 0.0;
 	AdcChannelConfig adcConfig;
 	adcConfig.InitSingle(hw.GetPin(21));
 
 	hw.adc.Init(&adcConfig, 1);
 	hw.adc.Start();
-	
-	while(1) {
-		for (size_t i = 0; i < NUMBER_OF_FLOATS; i++)
-		{
-			auto value = hw.adc.GetFloat(0);
-			store_value(value);
-			System::Delay(1);
-		}
-		avg = get_avg_from_ram();
-		value_count = 0;
-		// auto avg_falue = ReadAndLog(hw, 40) / 40.0;
-		// hw.PrintLine("cycle done %f", avg);
-		System::Delay(500);
+	hw.PrintLine("Adc Started, entering Loop");
+	while(1) {	
+		System::Delay(250);
+		collect_temperature();
+		get_avg_from_ram();
+		hw.PrintLine("ADC 1: %d", hw.adc.Get(0));
 	}
 }
